d3d0bd87c29f0b55c6b15d2cc7126cdae50a45b7
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-08-18 10:06:46 -0700

    added multiple phone numbers

--- app.js
+++ app.js
@@ -1,37 +1,81 @@
 'use strict';
 
-$( document ).ready(function() {
-    
+$(document).ready(function() {
 
-var contact = [{
-	firstName: " ",
-	lastName: " ",
-	phoneNumber: " ",
-	street: " ",
-	city: " ",
-	state: " "
-}]
+    var contacts = []
 
+    $('#submitForm').on("click", function(event) {
+        event.preventDefault();
+        var firstName = $('#firstName').val();
+        var lastName = $('#lastName').val();
 
-$('form').submit(function(event){
-	event.preventDefault();
-	var first = $('#firstName').val();
-	var lastName = $('#lastName').val();
-	var phoneNumber = $('#phoneNumber').val();
-	var street = $('#street').val();
-	var city = $('#city').val();
-	var state = $('#state').val();
-	// var contactArray = [firstName, lastName, phoneNumber, street, city, state];
-	
-	Object.create(contact);
+        var phoneNumber = $('#phoneNumber').val();
+        var street = $('#street').val();
+        var city = $('#city').val();
+        var state = $('#state').val();
+        var contact = {
+            firstName: firstName,
+            lastName: lastName,
+            phoneNumber: [],
+            // address : [{street: "some street", city: "some city"}]
+            street: street,
+            city: city,
+            state: state
+        }
+        var selectPhone = $('.addPhone input');
+        var length = selectPhone.length;
+        for (var i = 0; i < length; i++) {
+            var tempPhone = $(selectPhone[i]).val();
+            // more code needed
+            contact.phoneNumber.push(tempPhone)
+        }
 
-	console.log(contact.firstName);
+        // Index is used to identify location of contact in an array
+        var index = contacts.push(contact) - 1;
 
-	// console.log(contactArray);
+        $('#firstName').val("");
+        $('#lastName').val("");
+        $('#phoneNumber').val("");
+        $('#street').val("");
+        $('#city').val("");
+        $('#state').val("");
 
-});
+        $("#addressBook").append("<li><a href='#aside' class='contactList' id= '" + index + "'>" + contact.firstName + " " + contact.lastName + "</li>");
+    });
 
+    $('body').on('click', '.contactList', function(event) {
+        event.preventDefault();
+        console.log("hi");
+        console.log(contacts[this.id]);
+        $('.first-and-last-name-card').text(contacts[this.id].firstName + " " + contacts[this.id].lastName);
+        $('.first-name-card').text(contacts[this.id].firstName);
+        $('.last-name-card').text(contacts[this.id].lastName);
+        $('.phone-card').text(contacts[this.id].phoneNumber);
+        $('.address-card').text(contacts[this.id].street + " " + contacts[this.id].city + " " + contacts[this.id].state);
 
+    });
+
+    $('.additionalPhone').on('click', function(event) {
+        event.preventDefault();
+        $('.addPhone').append('<div class="newPhone"><input type="tel" class="form-control"><button class="removePhone btn-primary">Remove</button></br></div>');
+    });
+
+    $('body').on('click', '.removePhone', function(event) {
+        event.preventDefault();
+        // hints: this
+        $(this).parent().remove();
+        console.log($(this).text());
+        // $('.addPhone input:first').siblings().remove();
+        // $('.addPhone').append('<br>');
+    });
+    // Hints
+    // better ways
+    // .clone() jquery
+
+    // $('.additionalAddress').on('click', function(event) {
+    //     event.preventDefault();
+    //     $('.addAddress').append('<input type="tel" class="form-control"><br>');
+    // });
 
 
 
